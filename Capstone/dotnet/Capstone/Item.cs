71aeb4a67db705b414c01ee470faddb56b6cd827
Shane Weingartner <weingartners8324@gmail.com>
2021-06-03 13:39:04 -0400

    First commit

---
+++ Capstone/dotnet/Capstone/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    class Item
+    {
+        public string Identifier { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Type { get; set; }
+
+        public Item(string identifier, string name, double price, string type)
+        {
+            Identifier = identifier;
+            Name = name;
+            Price = price;
+            Type = type;
+
+
+        }
+
+
+
+    }
+}
